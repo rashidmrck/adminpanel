a5d21157482cb14c6fd7489e9067ed81129a554e
Mr CK <rashidmrck@gmail.com>
2021-07-15 02:47:05 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/adminmaster/adminpanel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adminmaster.adminpanel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/responsive.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Responsive extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
