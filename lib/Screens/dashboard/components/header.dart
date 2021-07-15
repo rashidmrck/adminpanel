4a1a6dbb8f5912a89b32d708d0edf5ed57addf95
Mr CK <rashidmrck@gmail.com>
2021-07-16 03:10:54 +0530

    .

---
+++ lib/Screens/dashboard/components/header.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../constants.dart';
+
+
+class Header extends StatelessWidget {
+  const Header({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Dashboard',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        Spacer(),
+        Expanded(
+          child: SearchFiled(),
+        ),
+        ProfileCard()
+      ],
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: defaultPadding),
+      padding: const EdgeInsets.symmetric(
+        horizontal: defaultPadding,
+        vertical: defaultPadding / 2,
+      ),
+      decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
+          ),
+          border: Border.all(color: Colors.white10)),
+      child: Row(
+        children: [
+          Image.asset(
+            'assets/images/profile_pic.png',
+            height: 38,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: defaultPadding / 2),
+            child: Text(
+              'Mr CK',
+            ),
+          ),
+          Icon(Icons.keyboard_arrow_down)
+        ],
+      ),
+    );
+  }
+}
+
+class SearchFiled extends StatelessWidget {
+  const SearchFiled({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Search..',
+        fillColor: secondaryColor,
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        suffixIcon: InkWell(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Container(
+            padding: const EdgeInsets.all(defaultPadding * 0.75),
+            margin: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: SvgPicture.asset('assets/icons/Search.svg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
